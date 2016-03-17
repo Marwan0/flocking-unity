4067b85d4fa3a6126ff840a2af4f6ba58667a16a
Tom Kluyskens <tom@mattermachine.com>
2016-03-17 14:42:46 +1300

    exposing max speed and radius

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class Main : MonoBehaviour {
 
     public int numberOfCows = 100;
+    public float maxRadius = 50;
+    public float maxSpeed = 3;
 
 	// Use this for initialization
 	void Start () {
 
         for ( int i = 0; i < numberOfCows; i++ ) {
             var cow = Instantiate(Resources.Load("cow")) as GameObject;
-            cow.GetComponent<MoveCow>().timeOffset = Random.Range(-10,10);
-            cow.GetComponent<MoveCow>().radius     = Random.Range(10,50);
-            cow.GetComponent<MoveCow>().speed      = Random.Range(-3.0f,3.0f);
+            cow.GetComponent<MoveCow>().timeOffset = Random.Range(-100,100);
+            cow.GetComponent<MoveCow>().radius     = Random.Range(10,maxRadius);
+            cow.GetComponent<MoveCow>().speed      = Random.Range(-maxSpeed,maxSpeed);
         }
 	}
 	
