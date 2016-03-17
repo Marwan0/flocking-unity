4a767746359923fb6905726e8814df6573a0a461
Tom Kluyskens <tom@mattermachine.com>
2016-03-17 14:11:28 +1300

    move cow in circle

---
+++ Assets/MoveCow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCow : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.position = new Vector3( 50*Mathf.Sin(Time.time), 0, 50*Mathf.Cos(Time.time) );
+
+    }
+}
