cfbbbdecb850287cca59688b56a6d6356ac40d7f
Tom Kluyskens <tom@mattermachine.com>
2016-03-17 14:32:31 +1300

    100 cows

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class Main : MonoBehaviour {
 
+    public int numberOfCows = 100;
+
 	// Use this for initialization
 	void Start () {
-        var cow = Instantiate(Resources.Load("cow")) as GameObject;
+
+        for ( int i = 0; i < numberOfCows; i++ ) {
+            var cow = Instantiate(Resources.Load("cow")) as GameObject;
+        }
 	}
 	
 	// Update is called once per frame
